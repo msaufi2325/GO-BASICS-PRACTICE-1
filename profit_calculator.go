790c0a8d6902300a5c553ad0e71154128e3a8f5c
Saufi <msaufi2325@gmail.com>
2023-12-24 13:54:58 +0900

    first commit

---
+++ profit_calculator.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Epenses: ")
+	taxRate := getUserInput("Tax rate in %: ")
+
+	earningBeforeTax, earningAfterTax, ratio := calculateEarnings(revenue, expenses, taxRate)
+
+	fmt.Println("Earning before tax: ", earningBeforeTax)
+	fmt.Println("Profit: ", earningAfterTax)
+	fmt.Println("Ratio: ", ratio)
+
+	formattedEarningBeforeTax := fmt.Sprintf("Earning before tax: %0.1f\n", earningBeforeTax)
+	formattedProfit := fmt.Sprintf("Profit: %0.1f\n", earningAfterTax)
+	formattedRatio := fmt.Sprintf("Ratio: %0.1f\n", ratio)
+
+	fmt.Print(formattedEarningBeforeTax, formattedProfit, formattedRatio)
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	return userInput
+}
+
+func calculateEarnings(revenue, expenses, taxRate float64) (earningBeforeTax float64, earningAfterTax float64, ratio float64) {
+	earningBeforeTax = revenue - expenses
+	earningAfterTax = earningBeforeTax * (1 - taxRate/100)
+	ratio = earningBeforeTax / earningAfterTax
+
+	return earningBeforeTax, earningAfterTax, ratio
+}
