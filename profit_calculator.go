e2888f5ba02a133cefbf1c13384be755d1a93d9b
Saufi <msaufi2325@gmail.com>
2023-12-25 23:17:24 +0900

    Sec 2 - end

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 // Goals
@@ -12,33 +13,12 @@ import (
 //   - Not 0
 // 2) Store calculated results into file.
 
-func main() {
-
-	revenue, err := getUserInput("Revenue: ")
-	expenses, err := getUserInput("Expenses: ")
-	taxRate, err := getUserInput("Tax rate in %: ")
-
-	if err != nil {
-		fmt.Println("ERROR!")
-		fmt.Println(err)
-		fmt.Println("---------------------------------")
-	}
-
-	earningBeforeTax, earningAfterTax, ratio := calculateEarnings(revenue, expenses, taxRate)
-
-	formattedEarningBeforeTax := fmt.Sprintf("Earning before tax: %0.1f\n", earningBeforeTax)
-	formattedProfit := fmt.Sprintf("Profit: %0.1f\n", earningAfterTax)
-	formattedRatio := fmt.Sprintf("Ratio: %0.1f\n", ratio)
-
-	fmt.Print(formattedEarningBeforeTax, formattedProfit, formattedRatio)
-}
-
 func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 	if userInput <= 0 {
-		return 0, errors.New("invalid amount must be greater than 0")
+		return 0, errors.New("value must be a positive number")
 	}
 
 	return userInput, nil
@@ -50,4 +30,45 @@ func calculateEarnings(revenue, expenses, taxRate float64) (earningBeforeTax flo
 	ratio = earningBeforeTax / earningAfterTax
 
 	return earningBeforeTax, earningAfterTax, ratio
+}
+
+func storeResults(earningBeforeTax, earningAfterTax, ratio float64) {
+	results := fmt.Sprintf("Earning Before Tax: %0.1f\nProfit: %0.1f\nRatio: %0.3f", earningBeforeTax, earningAfterTax, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func main() {
+
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------------EXITING-----------------")
+		return
+		// panic(err)
+	}
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------------EXITING-----------------")
+		return
+	}
+	taxRate, err := getUserInput("Tax rate in %: ")
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------------EXITING-----------------")
+		return
+	}
+
+	earningBeforeTax, earningAfterTax, ratio := calculateEarnings(revenue, expenses, taxRate)
+
+	storeResults(earningBeforeTax, earningAfterTax, ratio)
+
+	formattedEarningBeforeTax := fmt.Sprintf("Earning before tax: %0.1f\n", earningBeforeTax)
+	formattedProfit := fmt.Sprintf("Profit: %0.1f\n", earningAfterTax)
+	formattedRatio := fmt.Sprintf("Ratio: %0.1f\n", ratio)
+
+	fmt.Print(formattedEarningBeforeTax, formattedProfit, formattedRatio)
 }
